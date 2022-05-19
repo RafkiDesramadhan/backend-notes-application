f7cef2bef8bece372ca0355cb3814711ba2ad7fb
RafkiDesramadhan <rafki.desramadhan@gmail.com>
2022-05-19 21:31:01 +0700

    make it bettter

--- app.js
+++ app.js
@@ -46,7 +46,10 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60000 },
+    cookie: {
+      secure: true,
+      maxAge: 60000,
+    },
   })
 );
 app.use(flash());
