6011855500f4269a189fd9ff289909816a2a30f2
RafkiDesramadhan <rafki.desramadhan@gmail.com>
2022-05-19 20:36:41 +0700

    make it bettter

--- routes/api.js
+++ routes/api.js
@@ -23,7 +23,19 @@ router.delete("/logout", apiController.logout);
 router.get("/token", refreshToken);
 
 // notes
-router.get("/notes", verifyToken, apiController.getNotes);
+router.get(
+  "/notes",
+  function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://majestic-platypus-5b41a0.netlify.app"
+    );
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  },
+  verifyToken,
+  apiController.getNotes
+);
 router.get("/notes/:id", verifyToken, apiController.getNotesById);
 router.post("/notes", verifyToken, apiController.addNotes);
 router.delete("/notes/:id", verifyToken, apiController.deleteNotes);
